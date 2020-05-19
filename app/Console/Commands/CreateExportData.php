6ee06b966a021550478e495ef20064616e150dfd
Kris <git@k118.de>
2020-05-19 22:39:12 +0200

    Update CreateExportData.php

--- app/Console/Commands/CreateExportData.php
+++ app/Console/Commands/CreateExportData.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Company;
 use App\Device;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class CreateExportData extends Command
@@ -81,5 +82,42 @@ class CreateExportData extends Command
             }
             fclose($fp);
         }
+
+        $q = DB::table('devices')->join('scans', 'scans.bssid', '=', 'devices.bssid')
+            ->where('devices.vehicle_id', null)
+            ->where(function ($q) {
+                $q->where('scans.modified_vehicle_name', '<>', null)
+                    ->orWhere('scans.vehicle_name', '<>', null);
+            })
+            ->orderBy('devices.bssid', 'asc')
+            ->get();
+
+        $fp = fopen($exportPath . 'unsorted assignments.csv', 'w+');
+        fputcsv($fp, [
+            'bssid',
+            'possible_assignments',
+        ]);
+
+        $bssidList = [];
+        foreach ($q as $q1) {
+            $expl = $q1->modified_vehicle_name ?? $q1->vehicle_name;
+            $expl = str_replace('oder', ',', $expl);
+            foreach (explode(',', $expl) as $qRes) {
+                $qRes = trim($qRes);
+                if (!isset($bssidList[$q1->bssid]))
+                    $bssidList[$q1->bssid] = [];
+                if (!in_array($qRes, $bssidList[$q1->bssid]))
+                    $bssidList[$q1->bssid][] = $qRes;
+            }
+        }
+
+        foreach ($bssidList as $bssid => $data) {
+            sort($data);
+            fputcsv($fp, [
+                strtoupper($bssid),
+                implode(',', $data),
+            ]);
+        }
+        fclose($fp);
     }
 }
