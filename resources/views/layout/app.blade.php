c1ca9472043ee5807e0749a8acaa58706548416d
Kris <git@k118.de>
2020-05-19 13:20:24 +0200

    design improvements

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
 
     <title>{{__('Vehicle tracking')}}</title>
 
-    <link rel="stylesheet" href="/css/app.css" />
+    <link rel="stylesheet" href="/css/app.css"/>
     <style>
         :root {
             --jumbotron-padding-y: 3rem;
@@ -18,6 +18,7 @@
             margin-bottom: 0;
             background-color: #fff;
         }
+
         @media (min-width: 768px) {
             .jumbotron {
                 padding-top: calc(var(--jumbotron-padding-y) * 2);
@@ -46,7 +47,9 @@
             margin-bottom: .25rem;
         }
 
-        .box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
+        .box-shadow {
+            box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
+        }
     </style>
     <script src="/js/app.js"></script>
 
@@ -61,49 +64,54 @@
 
 <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">{{__('Vehicle tracking')}}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/verify/">{{ __('Verify') }}</a>
-                </li>
-            </ul>
-
-            <ul class="navbar-nav mr-auto">
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <div class="container">
+            <a class="navbar-brand" href="/">{{__('Vehicle tracking')}}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/verify/">{{ __('Verify') }}</a>
+                    </li>
+                </ul>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                <ul class="navbar-nav mr-right">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
         </div>
     </nav>
@@ -128,7 +136,13 @@
     </div>
 </footer>
 </body>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 </html>
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -3,90 +3,84 @@
 @section('content')
     <div class="row">
         <div class="col-md-8">
-            <div class="card mb-4 box-shadow">
+            <div class="card mb-4">
                 <div class="card-body">
                     <h5 class="card-title">{{__('Device')}}</h5>
                     <p>BSSID: {{$device->bssid}}</p>
                     <p>LastSeen: {{$device->lastSeen->isoFormat('DD.MM.YYYY HH:mm')}}</p>
                 </div>
             </div>
-            <div class="card mb-4 box-shadow">
+            <div class="card mb-4">
                 <div class="card-body">
                     <h5 class="card-title">{{__('Scans')}}</h5>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <table class="table">
-                            <thead>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>SSID</th>
+                            <th>Erfassung</th>
+                            <th></th>
+                            <th>Scantime</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($scans as $scan)
                             <tr>
-                                <th>SSID</th>
-                                <th>Erfassung</th>
-                                <th></th>
-                                <th>Scantime</th>
+                                <td>{{$scan->ssid}}</td>
+                                <td>
+                                    <form method="POST">
+                                        @csrf
+                                        <input type="hidden" name="modified_scan_id" value="{{$scan->id}}"/>
+                                        <input type="text" class="form-control" name="modified_vehicle_name"
+                                               value="{{$scan->modified_vehicle_name ?? $scan->vehicle_name}}"/>
+                                    </form>
+                                </td>
+                                <td>
+                                    @if($scan->modified_vehicle_name != NULL)
+                                        <i class="fas fa-info-circle" data-toggle="tooltip" data-placement="top"
+                                           title="Originale Erfassung: {{$scan->vehicle_name}}"></i>
+                                    @endif
+                                </td>
+                                <td>{{$scan->created_at->isoFormat('DD.MM.YYYY HH:mm')}}</td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($scans as $scan)
-                                <tr>
-                                    <td>{{$scan->ssid}}</td>
-                                    <td>
-                                        <form method="POST">
-                                            @csrf
-                                            <input type="hidden" name="modified_scan_id" value="{{$scan->id}}"/>
-                                            <input type="text" class="form-control" name="modified_vehicle_name"
-                                                   value="{{$scan->modified_vehicle_name ?? $scan->vehicle_name}}"/>
-                                        </form>
-                                    </td>
-                                    <td>
-                                        @if($scan->modified_vehicle_name != NULL)
-                                            <i class="fas fa-info-circle" data-toggle="tooltip" data-placement="top"
-                                               title="Originale Erfassung: {{$scan->vehicle_name}}"></i>
-                                        @endif
-                                    </td>
-                                    <td>{{$scan->created_at->isoFormat('DD.MM.YYYY HH:mm')}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
 
         <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
+            <div class="card mb-4">
                 <div class="card-body">
                     <h5 class="card-title">{{__('Verify Device')}}</h5>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <form method="post" accept-charset="utf-8">
-                            @csrf
-                            <input type="hidden" name="bssid" value="{{$device->bssid}}"/>
+                    <form method="post" accept-charset="utf-8">
+                        @csrf
+                        <input type="hidden" name="bssid" value="{{$device->bssid}}"/>
 
-                            <div class="form-group">
-                                <label>Unternehmen bzw. Kategorie</label>
-                                <select class="form-control" name="company_id">
-                                    @foreach($companies as $company)
-                                        <option value="{{$company->id}}">{{$company->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Bezeichnung</label>
-                                <input type="text" class="form-control" placeholder="Vehicle name"
-                                       name="vehicle_name">
-                            </div>
-                            <button type="submit" name="action" value="save" class="btn btn-info">Save</button>
-                            <button type="submit" name="action" value="notVerifiable" class="btn btn-danger">Not
-                                verifiable yet
-                            </button>
-                        </form>
-                    </div>
+                        <div class="form-group">
+                            <label>Unternehmen bzw. Kategorie</label>
+                            <select class="form-control" name="company_id">
+                                @foreach($companies as $company)
+                                    <option value="{{$company->id}}">{{$company->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Bezeichnung</label>
+                            <input type="text" class="form-control" placeholder="Vehicle name"
+                                   name="vehicle_name">
+                        </div>
+                        <button type="submit" name="action" value="save" class="btn btn-info">Save</button>
+                        <button type="submit" name="action" value="notVerifiable" class="btn btn-danger">Not
+                            verifiable yet
+                        </button>
+                    </form>
                 </div>
             </div>
 
-            <div class="card mb-4 box-shadow">
+            <div class="card mb-4">
                 <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p>Es gibt noch <b>{{$count}} Funknetze</b> zum zuordnen.</p>
-                    </div>
+                    <p>Es gibt noch <b>{{$count}} Funknetze</b> zum zuordnen.</p>
                 </div>
             </div>
         </div>
