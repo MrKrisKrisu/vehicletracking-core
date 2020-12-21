555652859292e87696b1ecda3f43569a30881baf
Kris <git@k118.de>
2020-12-21 21:36:38 +0100

    Removed unused endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\v1\ScanController;
-use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/vehicle/last_seen', [ApiController::class, 'getLastSeenVehicles']);
-Route::get('/vehicle/new', [ApiController::class, 'getNewVehicles']);
-
-Route::prefix('v1')->middleware(['auth.scan'])->group(function () {
+Route::prefix('v1')->middleware(['auth.scan'])->group(function() {
     Route::post('/scan', [ScanController::class, 'scan']);
 });
