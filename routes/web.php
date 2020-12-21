c2b5f2b65c4f33ef39b859758e28724baf5e1e61
Kris <git@k118.de>
2020-12-21 12:09:33 +0100

    Improved Routes

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,22 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', 'VehicleController@render');
-Route::post('/', 'VehicleController@saveVehicle');
-
-Route::get('/verify', 'VehicleController@verify');
-Route::post('/verify', 'VehicleController@saveVerify');
-
-Route::get("/stats", "StatController@renderStatpage");
-
-Route::get('/vehicle/{vehicle_id}', [
-    'uses' => 'VehicleController@renderVehicle',
-    'as' => 'vehicle'
-]);
-
-Route::get('/assign/', [
-    'uses' => 'VehicleController@assign',
-    'as' => 'assign'
-]);
-
-Route::post('/assign/', [
-    'uses' => 'VehicleController@saveAssignee',
-    'as' => 'saveAssignee'
-]);
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', [VehicleController::class, 'render']);
+    Route::post('/', [VehicleController::class, 'saveVehicle']);
+
+    Route::get('/verify', [VehicleController::class, 'verify']);
+    Route::post('/verify', [VehicleController::class, 'saveVerify']);
+
+    Route::get('/vehicle/{vehicle_id}', [VehicleController::class, 'renderVehicle'])->name('vehicle');
+
+    Route::get('/assign/', [VehicleController::class, 'assign'])->name('assign');
+    Route::post('/assign/', [VehicleController::class, 'saveAssignee'])->name('saveAssignee');
+});
+
+
