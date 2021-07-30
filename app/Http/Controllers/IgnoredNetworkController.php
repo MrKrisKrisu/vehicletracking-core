f4d2e0e8d1894d4c80841fc694eafa279eb80331
Kristian Stöckel <git@k118.de>
2021-07-30 16:03:56 +0200

    Fix bug

--- app/Http/Controllers/IgnoredNetworkController.php
+++ app/Http/Controllers/IgnoredNetworkController.php
@@ -6,6 +6,8 @@ use App\IgnoredNetwork;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Device;
+use App\Scan;
 
 class IgnoredNetworkController extends Controller {
     public function create(Request $request): JsonResponse {
@@ -14,7 +16,16 @@ class IgnoredNetworkController extends Controller {
                                             'contains' => ['nullable', 'gte:0', 'lte:1']
                                         ]);
         try {
-            $ignoredNetwork = IgnoredNetwork::create($validated);
+            if(in_array(strtolower($validated['ssid']), ['wifi@db', 'fahrgastfernsehen', 'uestra_regiobus_freewlan', 'wfb intern', 'westfalenbahn', 'wifionice', 'enno_wifi'])) {
+                abort(403);
+            }
+
+            $ignoredNetwork = IgnoredNetwork::firstOrCreate($validated);
+            Device::where('ssid', $validated['ssid'])
+                  ->update(['ignore' => 1]);
+            Scan::where('ssid', $validated['ssid'])
+                ->update(['hidden' => 1]);
+
             return response()->json(['success' => true, 'obj' => [
                 'ssid'     => $ignoredNetwork->ssid,
                 'contains' => $ignoredNetwork->contains
